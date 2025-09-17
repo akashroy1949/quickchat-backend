cfe0cc77c1e12e0d4726fc7f9429f78f7669c587
Akash <aroy933050@gmail.com>
2025-09-18 04:18:45 +0530

    feat(user): add online user status tracking
    - Add isOnline and lastSeen fields to User model
    - Implement getOnlineUsers controller to retrieve online users
    - Add /online route for fetching user online status
    - Update socket handler to manage online/offline status on connect/disconnect
    - Add getAllConnectedUsers utility in socket manager
    - Broadcast userOnline and userOffline events to clients

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   profileImage: { type: String, default: "" }, // URL of profile picture
+  isOnline: { type: Boolean, default: false }, // Online status
+  lastSeen: { type: Date, default: Date.now }, // Last seen timestamp
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
